dde498957f99ac17dc8c0ed0e7b7486ddb2dbaa6
unknown <peters0470@gmail.com>
2021-03-02 07:15:39 -0600

    added offline functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,53 @@
+const webpack = require("webpack");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 const path = require("path");
-const webpack = require('webpack');
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 
-module.exports = {
-    entry: {
-        app: "./assets/js/script.js",
-        events: "./assets/js/events.js",
-        schedule: "./assets/js/schedule.js",
-        tickets: "./assets/js/tickets.js"
-      },
-    output: {
-        filename: "[name].bundle.js",
-        path: __dirname + "/dist",
-      },
-      module: {
-          rules: [
-              {
-                  test: /\.jpg$/i,
-                  use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                          name (file) {
-                            return "[path][name].[ext]"
-                          },
-                          publicPath: function(url) {
-                            return url.replace("../", "/assets/")
-                          }
-                        }  
-                      },
-                      {
-                        loader: 'image-webpack-loader'
-                      }
-                    ]  
-              }
-          ]
-      },
-      plugins: [
-          new webpack.ProvidePlugin({
-              $: 'jquery',
-              jQuery: 'jquery'
-          }),
-          new BundleAnalyzerPlugin({
-              analyzerMode: "static", 
-          })
+const config = {
+  entry: {
+    app: './assets/js/script.js',
+    events: './assets/js/events.js',
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: __dirname + '/dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name (file) {
+              return '[path][name].[ext]'
+            },
+            publicPath: function(url) {
+                return url.replace('../', '/assets/')
+            },
+          }  
+        }, 
+        {
+          loader: 'image-webpack-loader',
+        },
       ],
-      mode: 'development'
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery"
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static"
+    }),
+ 
+  ],
+  mode: "development"
 };
 
-
+module.exports = config;
